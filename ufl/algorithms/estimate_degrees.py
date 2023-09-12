bf859ba99471580e41ec2885a5a67edc2a8322d9
ksagiyam <k.sagiyama@imperial.ac.uk>
2023-09-12 16:32:04 +0100

    k

--- ufl/algorithms/estimate_degrees.py
+++ ufl/algorithms/estimate_degrees.py
@@ -74,6 +74,10 @@ class SumDegreeEstimator(MultiFunction):
             d = self.default_degree
         return d
 
+    def subspace(self, v):
+        """A Subspace does not increase degree."""
+        return 0
+
     def _reduce_degree(self, v, f):
         """Reduces the estimated degree by one; used when derivatives
         are taken. Does not reduce the degree when TensorProduct elements
